bd7ccf18575c0530bf36dbd1744351db4ab0fcab
vco <victor.courtin@manaos.com>
2022-09-08 09:54:40 +0200

    add bibliothque & couture

---
+++ src/routes/blog/bibliotheque.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { SectionPlain } from '$lib/components';
+</script>
+
+<SectionPlain>
+  <h1>Bibliothèque</h1>
+  <p>
+    La bibliothèque s'ouvre à APBA et propose des livres en rapport avec nos activités, ainsi que la
+    possibilité d'utiliser votre carte membre de la bibliothèque du square Lulli pour emprunter un
+    livre dans nos locaux !
+  </p>
+</SectionPlain>
---
+++ src/routes/blog/couture.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import type { CarrouselPicture } from '$lib/components';
+import { Carrousel, SectionPlain } from '$lib/components';
+const pictures: CarrouselPicture[] = [
+  { alt: '', src: '/couture/sac-habille.jpg' },
+  { alt: '', src: '/couture/sac.jpg' },
+  { alt: '', src: '/couture/ambiance.jpg' },
+  { alt: '', src: '/couture/apprendre.jpg' },
+  { alt: '', src: '/couture/atelier.jpg' }
+];
+</script>
+
+<SectionPlain>
+  <h1>Coûture</h1>
+  <p>
+    L'association APBA propose des activités de coûture régulièrement dans ses locaux au square
+    Lulli. Nous vous donnons rendez-vous !
+  </p>
+  <p>
+    Au travers cette activité, vous pouvez apprendre à coudre, à broder, à border, faire des
+    aisances, des ourlets, des patronages...
+  </p>
+</SectionPlain>
+
+<Carrousel pictures="{pictures}" duration="{2000}" />
